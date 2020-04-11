a74b045b8ecdeaf68e21b137c7a961b758fb96cc
Kayode Adechinan <kayode.adechinan@gmail.com>
2020-04-11 11:07:59 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ export class App extends Component {
 
   componentDidMount() {
     this.fetchAdvice();
-
-   // setInterval(() => this.fetchAdvice(), 10000);
   }
 
   fetchAdvice = () => {
@@ -23,13 +21,27 @@ export class App extends Component {
   render() {
     const { advice } = this.state;
     return (
-      <div className="app">
-        <div className="card">
-          <h1 className="heading">{advice}</h1>
-
-          <button className="button" onClick={this.fetchAdvice}>
-            <span>GIVE ME ADVICE!</span>
-          </button>
+      <div className="bgimg w3-display-container w3-animate-opacity  w3-text-white">
+        <div className="w3-display-topleft w3-padding-large w3-xlarge">
+          Advice App
+        </div>
+        <div className="w3-display-middle">
+          <h1 className="w3-jumbo w3-animate-top">{advice}</h1>
+          <hr
+            className="w3-border-grey"
+            style={{ margin: "auto", width: "40%" }}
+          />
+          <p className="w3-large w3-center">
+            <button
+              class="w3-button w3-red w3-round-xlarge  w3-wide w3-ripple"
+              onClick={this.fetchAdvice}
+            >
+              <b>GIVE ME ADVICE !</b>
+            </button>
+          </p>
+        </div>
+        <div className="w3-display-bottomleft w3-padding-large">
+          Built with React.js
         </div>
       </div>
     );
